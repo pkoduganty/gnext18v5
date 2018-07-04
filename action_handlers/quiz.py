4b06e5695e8c11f59583ae60fd723a82ff97415f
Praveen Koduganty <pkoduganty@gmail.com>
2018-07-04 23:58:22 +0530

    display quiz report not done yet

--- action_handlers/quiz.py
+++ action_handlers/quiz.py
@@ -46,14 +46,21 @@ def list_all(session, request):
     elif len(quizzes)>1:
       response_text = random.choice(QUIZ_SELECT)
       context = OutputContext(session, OUT_CONTEXT_QUIZ, type=OUT_CONTEXT_QUIZ)
-      return Response(response_text).text(response_text).select(response_text, select_cards).outputContext(context).build()
+      return Response(response_text).text(response_text).select(response_text, select_cards) \
+              .outputContext(context).build()
   
   response_text='No quizzes'
   return Response(response_text).text(response_text).build()
 
 
 def start(session, request):
-  contexts, quiz, question, shuffled_question_ids, question_index, current_answer_correct, total_correct = getContexts(session, request)        
+  quizContext = getContexts(session, request)
+  
+  if quizContext is None:
+    error_text='Error, developer error, couldn\'t parse contexts'
+    return Response(error_text).text(error_text).build()
+  
+  contexts, quiz, question, shuffled_question_ids, question_index, is_correct, total_correct = quizContext
   
   if quiz is None:
     error_text = 'Error, quiz not found'  
@@ -63,11 +70,18 @@ def start(session, request):
   card = Card(quiz.title, description=description)
   response_text = quiz.title
   context = OutputContext(session, OUT_CONTEXT_QUIZ_DO, type=OUT_CONTEXT_QUIZ_DO, lifespan=2, id=quiz.id)
-  return Response(response_text).text(description).card(card).suggestions(['Yes','No']).setOutputContexts(contexts).outputContext(context).build()
+  return Response(response_text).text(description).card(card).suggestions(['Yes','No']) \
+          .setOutputContexts(contexts).outputContext(context).build()
 
 
 def start_yes(session, request):
-  contexts, quiz, question, shuffled_question_ids, question_index, current_answer_correct, total_correct = getContexts(session, request)
+  quizContext = getContexts(session, request)
+  
+  if quizContext is None:
+    error_text='Error, developer error, couldn\'t parse contexts'
+    return Response(error_text).text(error_text).build()
+  
+  contexts, quiz, question, shuffled_question_ids, question_index, is_correct, total_correct = quizContext
   
   if quiz is None:
     error_text = 'Error, developer error - quiz not found'  
@@ -86,11 +100,18 @@ def start_yes(session, request):
   context = OutputContext(session, OUT_CONTEXT_QUIZ_QUESTION, lifespan=1, 
                           shuffled=shuffled_question_ids, question_index=question_index, 
                           id=question.id, total_correct=0)
-  return Response(response_text).text(response_text).card(card).suggestions(question.choices).setOutputContexts(contexts).outputContext(context).build()
+  return Response(response_text).text(response_text).card(card).suggestions(question.choices) \
+          .setOutputContexts(contexts).outputContext(context).build()
   
 
 def answer(session, request):
-  contexts, quiz, question, shuffled_question_ids, question_index, current_answer_correct, total_correct = getContexts(session, request)
+  quizContext = getContexts(session, request)
+  
+  if quizContext is None:
+    error_text='Error, developer error, couldn\'t parse contexts'
+    return Response(error_text).text(error_text).build()
+  
+  contexts, quiz, question, shuffled_question_ids, question_index, is_correct, total_correct = quizContext
   
   if quiz is None:
     error_text = 'Error, developer error - quiz not found'  
@@ -103,19 +124,26 @@ def answer(session, request):
     
   if result is not None:
     logging.debug('Right answer {0}'.format(query))
-    current_answer_correct=True
+    is_correct=True
     total_correct += 1
   else:
     logging.debug('Incorrect answer {0}, should be {1}'.format(query, str(question.answers)))
   
   context = OutputContext(session, OUT_CONTEXT_QUIZ_QUESTION, lifespan=1, 
                           shuffled=shuffled_question_ids, question_index=int(question_index), 
-                          id=question.id, answer=query, is_correct=current_answer_correct, total_correct=total_correct)
-  return Response('Followup to next question').followupEvent(EVENT_NEXT_QUESTION).setOutputContexts(contexts).outputContext(context).build()
+                          id=question.id, answer=query, is_correct=is_correct, total_correct=total_correct)
+  return Response('Followup to next question').followupEvent(EVENT_NEXT_QUESTION) \
+          .setOutputContexts(contexts).outputContext(context).build()
   
 
 def next_question(session, request):
-  contexts, quiz, question, shuffled_question_ids, question_index = getContexts(session, request)
+  quizContext = getContexts(session, request)
+  
+  if quizContext is None:
+    error_text='Error, developer error, couldn\'t parse contexts'
+    return Response(error_text).text(error_text).build()
+  
+  contexts, quiz, question, shuffled_question_ids, question_index, is_correct, total_correct = quizContext
   
   if quiz is None:
     error_text = 'Error, developer error - quiz not found'  
@@ -127,23 +155,20 @@ def next_question(session, request):
   else:
     logging.debug('skipped question {0} - {1}', question.id, question.question)
 
-  result = getPrevQuestionResult(contexts, quiz)
-  if result is None:
-    error='Error, developer error - previous question context not found'
-    return Response(error).text(error).build()
-  
-  questionId, question, student_answer, isCorrect, totalCorrect = result 
   prev_question_result=''
-  if bool(isCorrect):
+  if bool(is_correct):
     prev_question_result=random.choice(CORRECT_ANSWER).format(question.question, question.answers[0])
-    totalCorrect += 1 #TODO have question specific points
+    total_correct += 1 #TODO have question specific points
   else:
     prev_question_result=random.choice(INCORRECT_ANSWER).format(question.question, question.answers[0])
-  
+    
   question_index += 1 #next question
   if question_index >= len(quiz.questions):
-    logging.debug('quiz end, printing report')          
-    return Response('Quiz Completed').resetContexts().text(prev_question_result).text(random.choice(QUIZ_REPORT).format(totalCorrect, len(quiz.questions))).suggestions(WELCOME_SUGGESTIONS).build()
+    logging.debug('quiz end, printing report')
+    #reset output contexts
+    return Response('Quiz Completed').text(prev_question_result).setOutputContexts([]) \
+            .text(random.choice(QUIZ_REPORT).format(total_correct, len(quiz.questions))) \
+            .suggestions(WELCOME_SUGGESTIONS).build()
   else: 
     question = quiz.questions[question_index]
     logging.debug('display new question {0} - {1}', question.id, question.question)
@@ -153,8 +178,10 @@ def next_question(session, request):
     response_text = title +' : \n' + question.question
     context = OutputContext(session, OUT_CONTEXT_QUIZ_QUESTION, lifespan=1, 
                           shuffled=shuffled_question_ids, question_index=question_index, 
-                          id=question.id,)
-    return Response(response_text).text(prev_question_result).card(card).suggestions(question.choices).setOutputContexts(contexts).outputContext(context).build()
+                          id=question.id, total_correct=total_correct)
+    return Response(response_text).text(prev_question_result).card(card) \
+            .suggestions(question.choices).setOutputContexts(contexts) \
+            .outputContext(context).build()
 
 def getPrevQuestionResult(contexts, quiz):
   for context in contexts:
@@ -178,10 +205,14 @@ def getQuestionById(questions, id):
 
 def getContexts(session, request):
   contexts=[]
+  quiz=None
   quizId=None
   shuffled_question_ids=None
   question_index=0
+  question=None
   questionId=None
+  is_correct=False
+  total_correct=0
   
   for context in request.get('queryResult').get('outputContexts'):
     logging.debug('context: {0}, parameters: {1}', context.get('name'), str(context.get('parameters')))
@@ -196,20 +227,23 @@ def getContexts(session, request):
       shuffled_question_ids=context.get('parameters').get('shuffled')
       question_index=context.get('parameters').get('question_index')
       question_index=int(question_index) if question_index is not None else 0
+      is_correct=bool(context.get('parameters').get('is_correct'))      
+      total_correct=context.get('parameters').get('total_correct')
+      total_correct=int(total_correct) if total_correct is not None else 0
 
   if quizId is None or not quizId.strip():
-    return (contexts, None, None, None, 0)
+    return (contexts, quiz, question, shuffled_question_ids, question_index, is_correct, total_correct)
   
   quiz = sample_homeworks.activity_id_dict.get(quizId) #from homework
   if quiz is None:
     quiz = sample_lessons.activity_id_dict.get(quizId) #from lesson
 
   if quiz is None:
-    return (contexts, None, None, None, 0)
+    return (contexts, quiz, question, shuffled_question_ids, question_index, is_correct, total_correct)
 
   question=None
   if questionId is not None and shuffled_question_ids is not None:
     question = getQuestionById(quiz.questions, shuffled_question_ids[question_index])  
   
   logging.info('quiz - %s, question - %s, question_index - %d, shuffled questions - %s', quizId, questionId, question_index, str(shuffled_question_ids))
-  return (contexts, quiz, question, shuffled_question_ids, question_index)
+  return (contexts, quiz, question, shuffled_question_ids, question_index, is_correct, total_correct)
